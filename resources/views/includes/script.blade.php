00a6166158258b43813732d03410535607389712
chzkyy <ftrizkyy@gmail.com>
2021-05-21 01:53:10 +0700

    first commit

---
+++ resources/views/includes/script.blade.php
@@ -0,0 +1,3 @@
+<script src="{{ url('frontend/lib/retina/retina.min.js') }}"></script>
+<script src="{{ url('frontend/lib/jquery/jquery-3.4.1.min.js') }}"></script>
+<script src="{{ url('frontend/lib/bootstrap/js/bootstrap.js') }}"></script>
